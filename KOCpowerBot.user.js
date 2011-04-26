5cebc15395ba7c571b24aea9d69111943d3311d4
dentox@gmail.com <dentox@gmail.com@91a980a7-b36c-2723-9ba7-67aa146be212>
2011-04-26 21:00:17 +0000

    Added option : lasttransport.
    git-svn-id: http://koc-power-bot.googlecode.com/svn/trunk@295 91a980a7-b36c-2723-9ba7-67aa146be212

--- KOCpowerBot.user.js
+++ KOCpowerBot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           KOC Power Bot
-// @version        20110426a
+// @version        20110426b
 // @namespace      mat
 // @homepage       http://userscripts.org/scripts/show/101052
 // @include        http://*.kingdomsofcamelot.com/*main_src.php*
@@ -10,7 +10,7 @@
 // ==/UserScript==
 
 
-var Version = '20110426a';
+var Version = '20110426b';
 
 // These switches are for testing, all should be set to false for released version:
 var DEBUG_TRACE = false;
@@ -135,6 +135,7 @@ var Options = {
   hideOnGoto   : true,
   transportinterval : 60,
   minwagons		:100,
+  lasttransport:0,
 };
 //unsafeWindow.pt_Options=Options;
 
