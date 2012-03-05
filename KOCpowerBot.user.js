7ac97967873abeb36ea3ea8d8c86dc360d184afe
darren780@yahoo.com <darren780@yahoo.com@91a980a7-b36c-2723-9ba7-67aa146be212>
2012-03-05 08:45:39 +0000

    updated version info
    git-svn-id: http://koc-power-bot.googlecode.com/svn/trunk@771 91a980a7-b36c-2723-9ba7-67aa146be212

--- KOCpowerBot.user.js
+++ KOCpowerBot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           KOC Power Bot
-// @version        20120209a
+// @version        20120305a
 // @namespace      mat
 // @homepage       http://userscripts.org/scripts/show/101052
 // @include        *.kingdomsofcamelot.com/*main_src.php*
@@ -16,7 +16,7 @@
 // ==/UserScript==
 
 
-var Version = '20120209a';
+var Version = '20120305a';
 
 // These switches are for testing, all should be set to false for released version:
 var DEBUG_TRACE = false;
