ae71fc564dd16cc82d96c5dd9e31c637e39dae0a
darren780@yahoo.com <darren780@yahoo.com@91a980a7-b36c-2723-9ba7-67aa146be212>
2012-03-15 02:13:05 +0000

    re fixed throne delete wrong items bug
    git-svn-id: http://koc-power-bot.googlecode.com/svn/trunk@811 91a980a7-b36c-2723-9ba7-67aa146be212

--- KOCpowerBot.user.js
+++ KOCpowerBot.user.js
@@ -7975,7 +7975,7 @@ Tabs.Options = {
         <TR><TD><INPUT id=deletes1toggle type=checkbox /></td><TD> Auto delete wild reports</td></tr>\
         <TR><TD><INPUT id=deletes2toggle type=checkbox /></td><TD> Auto delete crest target regardless of type</td></tr>\
 		<tr><td colspan=2><br><b>Throne Room Auto Salvage:</td></tr>\
-        <TR><TD><INPUT id=deletethrone type=checkbox '+ (Options.ThroneDeleteItems?'CHECKED ':'') +'/></td><TD> Auto delete throne items below '+ htmlSelector({0:'----', 1:'Simple', 2:'Common', 3:'Uncommon', 4:'Rare', 5:'Epic', 6:'Wonderous'},Options.ThroneDeleteLevel,'id=selecttil') +'</td></tr>\
+        <TR><TD><INPUT id=deletethrone type=checkbox '+ (Options.ThroneDeleteItems?'CHECKED ':'') +'/></td><TD> Auto delete throne items below '+ htmlSelector({0:'-----', 1:'Common', 2:'Uncommon', 3:'Rare', 4:'Epic', 5:'Wonderous'},Options.ThroneDeleteLevel,'id=selecttil') +'</td></tr>\
 		<tr><td>&nbsp;&nbsp;&nbsp-</td><td>Save the first: <INPUT id=throneSaveNum type=text size=2 maxlength=3 \> items</td></tr>\
 		<tr><td>&nbsp;&nbsp;&nbsp-</td><td>Save Range items: '+ htmlSelector({0:'----', 1:'All Range', 5:'Only Best and Active Range', 8:'Top 2 and Active Range'},Options.RangeSaveModeSetting,'id=selectRangeSaveMode') + '<span style="color:#800; font-weight:bold"><sup> &nbsp *Saves range items</sup></span></td></tr>\
         <tr><td>&nbsp;&nbsp;&nbsp-</td><td></b>Bot Deleted: &nbsp;' + Options.throneDeletedNum + '&nbsp; Items &nbsp; <span style="color:#800; font-weight:bold"><sup>*Updates On Refresh</sup></span></td></tr>\
