6f82af553dabb94e5995cb3f8a8a88c9e8ec84c7
tonyboyKOC@gmail.com <tonyboyKOC@gmail.com@91a980a7-b36c-2723-9ba7-67aa146be212>
2012-10-13 19:11:10 +0000

    forgot version number AGAIN! sorry.
    git-svn-id: http://koc-power-bot.googlecode.com/svn/trunk@1039 91a980a7-b36c-2723-9ba7-67aa146be212

--- KOCpowerBot.user.js
+++ KOCpowerBot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           KOC Power Bot
-// @version        20121012b
+// @version        20121013a
 // @namespace      mat
 // @homepage       http://userscripts.org/scripts/show/101052
 // @include        *.kingdomsofcamelot.com/*main_src.php*
@@ -17,7 +17,7 @@
 // ==/UserScript==
 
 
-var Version = '20121012b';
+var Version = '20121013a';
 
 // These switches are for testing, all should be set to false for released version:
 var DEBUG_TRACE = false;
