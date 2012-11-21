680d90c7f60d271909e54e6f3efbea1162f2af8e
jontey88@gmail.com <jontey88@gmail.com@91a980a7-b36c-2723-9ba7-67aa146be212>
2012-11-21 08:29:02 +0000

    Fixed refresh issue Removed gifts tab Removed unused code Minor bug fixes
    git-svn-id: http://koc-power-bot.googlecode.com/svn/trunk@1114 91a980a7-b36c-2723-9ba7-67aa146be212

---
+++ Craft_constants.js
@@ -0,0 +1,650 @@
+ /******* Crafting List ******
+(array) 1 = [object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object]
+    (object) 0 = [object Object]
+      (string) recipe_id = 1
+      (string) name = Crystal Song
+      (string) output_item_id = 3000
+      (null) consolation_item_id: null = null
+      (string) category = 1
+      (object) input = [object Object]
+        (array) items =
+
+        (object) resources = [object Object]
+          (string) 1 = 10500
+
+
+      (object) requirements = [object Object]
+        (string) building = 1
+
+      (string) insurance_cost = 5
+      (string) failure_chance = low
+
+    (object) 1 = [object Object]
+      (string) recipe_id = 2
+      (string) name = Tricked Wind
+      (string) output_item_id = 3001
+      (null) consolation_item_id: null = null
+      (string) category = 1
+      (object) input = [object Object]
+        (array) items =
+
+        (object) resources = [object Object]
+          (string) 1 = 5200
+
+
+      (object) requirements = [object Object]
+        (string) building = 1
+
+      (string) insurance_cost = 5
+      (string) failure_chance = low
+
+    (object) 2 = [object Object]
+      (string) recipe_id = 4
+      (string) name = Spun Bone
+      (string) output_item_id = 3003
+      (null) consolation_item_id: null = null
+      (string) category = 1
+      (object) input = [object Object]
+        (array) items =
+
+        (object) resources = [object Object]
+          (string) 1 = 10500
+
+
+      (object) requirements = [object Object]
+        (string) building = 1
+
+      (string) insurance_cost = 5
+      (string) failure_chance = low
+
+    (object) 3 = [object Object]
+      (string) recipe_id = 8
+      (string) name = Sewn Blood
+      (string) output_item_id = 3007
+      (null) consolation_item_id: null = null
+      (string) category = 1
+      (object) input = [object Object]
+        (array) items =
+
+        (object) resources = [object Object]
+          (string) 1 = 2500
+
+
+      (object) requirements = [object Object]
+        (string) building = 1
+
+      (string) insurance_cost = 5
+      (string) failure_chance = low
+
+    (object) 4 = [object Object]
+      (string) recipe_id = 9
+      (string) name = Sewn Weight
+      (string) output_item_id = 3008
+      (null) consolation_item_id: null = null
+      (string) category = 1
+      (object) input = [object Object]
+        (array) items =
+
+        (object) resources = [object Object]
+          (string) 1 = 2500
+
+
+      (object) requirements = [object Object]
+        (string) building = 1
+
+      (string) insurance_cost = 5
+      (string) failure_chance = low
+
+    (object) 5 = [object Object]
+      (string) recipe_id = 10
+      (string) name = Shape Muck
+      (string) output_item_id = 3009
+      (null) consolation_item_id: null = null
+      (string) category = 1
+      (object) input = [object Object]
+        (array) items =
+
+        (object) resources = [object Object]
+          (string) 1 = 2500
+
+
+      (object) requirements = [object Object]
+        (string) building = 1
+
+      (string) insurance_cost = 5
+      (string) failure_chance = low
+
+    (object) 6 = [object Object]
+      (string) recipe_id = 12
+      (string) name = Knit Grace
+      (string) output_item_id = 3011
+      (string) consolation_item_id = 3000
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3000 = 1
+          (string) 3001 = 2
+
+        (object) resources = [object Object]
+          (string) 1 = 21000
+
+
+      (object) requirements = [object Object]
+        (string) building = 1
+
+      (string) insurance_cost = 5
+      (string) failure_chance = low
+
+    (object) 7 = [object Object]
+      (string) recipe_id = 11
+      (string) name = Cast Hide
+      (string) output_item_id = 3010
+      (string) consolation_item_id = 1107
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 1107 = 1
+          (string) 3002 = 2
+
+        (object) resources = [object Object]
+          (string) 1 = 75000
+
+
+      (object) requirements = [object Object]
+        (string) building = 5
+
+      (string) insurance_cost = 5
+      (string) failure_chance = low
+
+    (object) 8 = [object Object]
+      (string) recipe_id = 25
+      (string) name = Knit Courage
+      (string) output_item_id = 221
+      (string) consolation_item_id = 3003
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3003 = 2
+          (string) 3007 = 5
+
+        (object) resources = [object Object]
+          (string) 1 = 50000
+
+
+      (object) requirements = [object Object]
+        (string) building = 5
+
+      (string) insurance_cost = 33
+      (string) failure_chance = med
+
+    (object) 9 = [object Object]
+      (string) recipe_id = 26
+      (string) name = Knit Wisdom
+      (string) output_item_id = 231
+      (string) consolation_item_id = 3003
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3003 = 2
+          (string) 3009 = 5
+
+        (object) resources = [object Object]
+          (string) 1 = 50000
+
+
+      (object) requirements = [object Object]
+        (string) building = 5
+
+      (string) insurance_cost = 33
+      (string) failure_chance = med
+
+    (object) 10 = [object Object]
+      (string) recipe_id = 27
+      (string) name = Knit Beauty
+      (string) output_item_id = 211
+      (string) consolation_item_id = 3000
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3000 = 2
+          (string) 3007 = 5
+
+        (object) resources = [object Object]
+          (string) 1 = 50000
+
+
+      (object) requirements = [object Object]
+        (string) building = 5
+
+      (string) insurance_cost = 33
+      (string) failure_chance = med
+
+    (object) 11 = [object Object]
+      (string) recipe_id = 28
+      (string) name = Knit Toil
+      (string) output_item_id = 241
+      (string) consolation_item_id = 3000
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3000 = 2
+          (string) 3009 = 5
+
+        (object) resources = [object Object]
+          (string) 1 = 50000
+
+
+      (object) requirements = [object Object]
+        (string) building = 5
+
+      (string) insurance_cost = 33
+      (string) failure_chance = med
+
+    (object) 12 = [object Object]
+      (string) recipe_id = 5
+      (string) name = Divine Act
+      (string) output_item_id = 3004
+      (string) consolation_item_id = 3010
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3000 = 3
+          (string) 3003 = 3
+          (string) 3010 = 1
+
+        (object) resources = [object Object]
+          (string) 1 = 120000
+
+
+      (object) requirements = [object Object]
+        (string) building = 6
+
+      (string) insurance_cost = 20
+      (string) failure_chance = med
+
+    (object) 13 = [object Object]
+      (string) recipe_id = 29
+      (string) name = Forge Fury
+      (string) output_item_id = 2002
+      (string) consolation_item_id = 3003
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3003 = 7
+          (string) 3007 = 21
+
+        (object) resources = [object Object]
+          (string) 1 = 400000
+
+
+      (object) requirements = [object Object]
+        (string) building = 6
+
+      (string) insurance_cost = 40
+      (string) failure_chance = high
+
+    (object) 14 = [object Object]
+      (string) recipe_id = 6
+      (string) name = Divine Rite
+      (string) output_item_id = 3005
+      (string) consolation_item_id = 3004
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3000 = 3
+          (string) 3003 = 3
+          (string) 3004 = 1
+
+        (object) resources = [object Object]
+          (string) 1 = 180000
+
+
+      (object) requirements = [object Object]
+        (string) building = 7
+
+      (string) insurance_cost = 40
+      (string) failure_chance = high
+
+    (object) 15 = [object Object]
+      (string) recipe_id = 7
+      (string) name = Divine Toll
+      (string) output_item_id = 3006
+      (string) consolation_item_id = 3005
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3000 = 3
+          (string) 3003 = 3
+          (string) 3005 = 1
+
+        (object) resources = [object Object]
+          (string) 1 = 240000
+
+
+      (object) requirements = [object Object]
+        (string) building = 8
+
+      (string) insurance_cost = 40
+      (string) failure_chance = high
+
+    (object) 16 = [object Object]
+      (string) recipe_id = 13
+      (string) name = Divine Song
+      (string) output_item_id = 401
+      (string) consolation_item_id = 3006
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3000 = 3
+          (string) 3003 = 3
+          (string) 3006 = 1
+
+        (object) resources = [object Object]
+          (string) 1 = 320000
+
+
+      (object) requirements = [object Object]
+        (string) building = 9
+
+      (string) insurance_cost = 40
+      (string) failure_chance = high
+
+    (object) 17 = [object Object]
+      (string) recipe_id = 14
+      (string) name = Divine Hymn
+      (string) output_item_id = 402
+      (string) consolation_item_id = 401
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3000 = 3
+          (string) 3003 = 3
+          (string) 401 = 1
+
+        (object) resources = [object Object]
+          (string) 1 = 500000
+
+
+      (object) requirements = [object Object]
+        (string) building = 10
+
+      (string) insurance_cost = 124
+      (string) failure_chance = high
+
+    (object) 18 = [object Object]
+      (string) recipe_id = 31
+      (string) name = Mist Song
+      (string) output_item_id = 1120
+      (string) consolation_item_id = 1110
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 1110 = 1
+          (string) 261 = 1
+          (string) 3007 = 7
+          (string) 3011 = 2
+
+        (object) resources = [object Object]
+          (string) 1 = 80000
+
+
+      (object) requirements = [object Object]
+        (string) building = 10
+
+      (string) insurance_cost = 18
+      (string) failure_chance = low
+
+    (object) 19 = [object Object]
+      (string) recipe_id = 32
+      (string) name = Mist Dirge
+      (string) output_item_id = 1121
+      (string) consolation_item_id = 1120
+      (string) category = 1
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 1120 = 1
+          (string) 272 = 1
+          (string) 3009 = 3
+          (string) 3008 = 3
+          (string) 3011 = 2
+
+        (object) resources = [object Object]
+          (string) 1 = 85000
+
+
+      (object) requirements = [object Object]
+        (string) building = 10
+
+      (string) insurance_cost = 20
+      (string) failure_chance = med
+
+
+  (array) 3 = [object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object]
+    (object) 0 = [object Object]
+      (string) recipe_id = 15
+      (string) name = Crimson Act
+      (string) output_item_id = 261
+      (string) consolation_item_id = 3007
+      (string) category = 3
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3007 = 5
+
+        (object) resources = [object Object]
+          (string) 1 = 10000
+
+
+      (object) requirements = [object Object]
+        (string) building = 2
+
+      (string) insurance_cost = 9
+      (string) failure_chance = low
+
+    (object) 1 = [object Object]
+      (string) recipe_id = 18
+      (string) name = Aegis Act
+      (string) output_item_id = 271
+      (string) consolation_item_id = 3008
+      (string) category = 3
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3008 = 5
+
+        (object) resources = [object Object]
+          (string) 1 = 10000
+
+
+      (object) requirements = [object Object]
+        (string) building = 2
+
+      (string) insurance_cost = 9
+      (string) failure_chance = low
+
+    (object) 2 = [object Object]
+      (string) recipe_id = 20
+      (string) name = Trapped Wind
+      (string) output_item_id = 55
+      (string) consolation_item_id = 1103
+      (string) category = 3
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 1103 = 1
+          (string) 3001 = 5
+
+        (object) resources = [object Object]
+          (string) 1 = 25000
+
+
+      (object) requirements = [object Object]
+        (string) building = 2
+
+      (string) insurance_cost = 5
+      (string) failure_chance = low
+
+    (object) 3 = [object Object]
+      (string) recipe_id = 22
+      (string) name = Trick Hunger
+      (string) output_item_id = 273
+      (string) consolation_item_id = 3000
+      (string) category = 3
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 3000 = 5
+
+        (object) resources = [object Object]
+          (string) 1 = 160000
+
+
+      (object) requirements = [object Object]
+        (string) building = 2
+
+      (string) insurance_cost = 33
+      (string) failure_chance = med
+
+    (object) 4 = [object Object]
+      (string) recipe_id = 16
+      (string) name = Crimson Rite
+      (string) output_item_id = 262
+      (string) consolation_item_id = 261
+      (string) category = 3
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 261 = 1
+          (string) 3007 = 3
+
+        (object) resources = [object Object]
+          (string) 1 = 30000
+
+
+      (object) requirements = [object Object]
+        (string) building = 3
+
+      (string) insurance_cost = 43
+      (string) failure_chance = med
+
+    (object) 5 = [object Object]
+      (string) recipe_id = 19
+      (string) name = Aegis Rite
+      (string) output_item_id = 272
+      (string) consolation_item_id = 271
+      (string) category = 3
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 271 = 1
+          (string) 3008 = 3
+
+        (object) resources = [object Object]
+          (string) 1 = 30000
+
+
+      (object) requirements = [object Object]
+        (string) building = 3
+
+      (string) insurance_cost = 43
+      (string) failure_chance = med
+
+    (object) 6 = [object Object]
+      (string) recipe_id = 21
+      (string) name = Poached Wind
+      (string) output_item_id = 57
+      (string) consolation_item_id = 55
+      (string) category = 3
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 55 = 1
+          (string) 3001 = 3
+
+        (object) resources = [object Object]
+          (string) 1 = 75000
+
+
+      (object) requirements = [object Object]
+        (string) building = 3
+
+      (string) insurance_cost = 20
+      (string) failure_chance = med
+
+    (object) 7 = [object Object]
+      (string) recipe_id = 23
+      (string) name = Trap Hunger
+      (string) output_item_id = 274
+      (string) consolation_item_id = 273
+      (string) category = 3
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 273 = 1
+          (string) 3000 = 3
+
+        (object) resources = [object Object]
+          (string) 1 = 240000
+
+
+      (object) requirements = [object Object]
+        (string) building = 3
+
+      (string) insurance_cost = 60
+      (string) failure_chance = med
+
+    (object) 8 = [object Object]
+      (string) recipe_id = 17
+      (string) name = Crimson Toll
+      (string) output_item_id = 280
+      (string) consolation_item_id = 262
+      (string) category = 3
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 262 = 1
+          (string) 3007 = 3
+
+        (object) resources = [object Object]
+          (string) 1 = 90000
+
+
+      (object) requirements = [object Object]
+        (string) building = 4
+
+      (string) insurance_cost = 40
+      (string) failure_chance = high
+
+    (object) 9 = [object Object]
+      (string) recipe_id = 24
+      (string) name = Poach Hunger
+      (string) output_item_id = 275
+      (string) consolation_item_id = 274
+      (string) category = 3
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 274 = 1
+          (string) 3000 = 3
+
+        (object) resources = [object Object]
+          (string) 1 = 320000
+
+
+      (object) requirements = [object Object]
+        (string) building = 4
+
+      (string) insurance_cost = 168
+      (string) failure_chance = high
+
+    (object) 10 = [object Object]
+      (string) recipe_id = 30
+      (string) name = Aegis Toll
+      (string) output_item_id = 281
+      (string) consolation_item_id = 272
+      (string) category = 3
+      (object) input = [object Object]
+        (object) items = [object Object]
+          (string) 272 = 1
+          (string) 3008 = 3
+
+        (object) resources = [object Object]
+          (string) 1 = 90000
+
+
+      (object) requirements = [object Object]
+        (string) building = 4
+
+      (string) insurance_cost = 40
+      (string) failure_chance = high
+*******/
