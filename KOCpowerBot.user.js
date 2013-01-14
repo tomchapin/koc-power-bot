b3223b22b31d7c3cc5b01b6016f8be3048930d1c
baos780@gmail.com <baos780@gmail.com@91a980a7-b36c-2723-9ba7-67aa146be212>
2013-01-14 10:16:17 +0000

    bugfix where autodf would use up all slots
    git-svn-id: http://koc-power-bot.googlecode.com/svn/trunk@1210 91a980a7-b36c-2723-9ba7-67aa146be212

--- KOCpowerBot.user.js
+++ KOCpowerBot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           KOC Power Bot
-// @version        20130114b
+// @version        20130114c
 // @namespace      mat
 // @homepage       http://userscripts.org/scripts/show/101052
 // @include        *.kingdomsofcamelot.com/*main_src.php*
@@ -34,7 +34,7 @@ if(window.self.location != window.top.location){
 	}
 }
 
-var Version = '20130114b';
+var Version = '20130114c';
 
 
 //bandaid to stop loading in advertisements containing the @include urls
@@ -10084,8 +10084,7 @@ Tabs.Barb = {
        document.getElementById(element1).innerHTML = 'Sent: ' + AttackOptions.BarbsDone[city];
        var element2 = 'pddataarray'+(city-1);
        document.getElementById(element2).innerHTML =  'RP: (' + slots + '/' + March.getTotalSlots(citynumber) +')';
-       
-       if ((slots-AttackOptions.RallyClip) <= 0) return;
+       if (Number(Number(slots)-Number(March.getTotalSlots(citynumber))) <= Number(AttackOptions.RallyClip)) return;
        if (t.knt.toSource() == "[]") return;
        var kid = t.knt[0].ID;
        
