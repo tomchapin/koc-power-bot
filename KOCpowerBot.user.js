62cac0db49669dafac6fbb4723cbb94b9da094b9
baos780@gmail.com <baos780@gmail.com@91a980a7-b36c-2723-9ba7-67aa146be212>
2013-03-24 19:30:56 +0000

    upgrade to darkforest handling of captcha's
    git-svn-id: http://koc-power-bot.googlecode.com/svn/trunk@1326 91a980a7-b36c-2723-9ba7-67aa146be212

--- KOCpowerBot.user.js
+++ KOCpowerBot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           KOC Power Bot
-// @version        20130321e
+// @version        20130324a
 // @namespace      mat
 // @homepage       http://userscripts.org/scripts/show/101052
 // @include        *.kingdomsofcamelot.com/*main_src.php*
@@ -34,7 +34,7 @@ if(window.self.location != window.top.location){
    }
 }
 
-var Version = '20130321e';
+var Version = '20130324a';
 
 //bandaid to stop loading in advertisements containing the @include urls
 if(document.URL.indexOf('sharethis') != -1) {
@@ -10108,6 +10108,7 @@ Tabs.Barb = {
   lookup:1,
   city:1,
   deleting:false,
+  waittime: 0,
   troopDef : [
       ['Supply', 1],
       ['Miltia', 2],
@@ -10500,7 +10501,11 @@ Tabs.Barb = {
       //Only send DF if city is not over 750K astone:: rewritten I want df's to farm items and level knights.. who cares about aetherstone?  -baos
       if (Seed.resources[cityID]["rec5"][0] > Number(AttackOptions.threshold)) {
          return;
-      }
+      };
+      //We've been captcha'd.  waiting 2 mins.
+      if(t.waittime > unsafeWindow.unixtime()) {
+		  return;
+	   };
        var element1 = 'pddatacity'+(city-1);
        document.getElementById(element1).innerHTML = 'Sent: ' + AttackOptions.BarbsDone[city];
        var element2 = 'pddataarray'+(city-1);
@@ -10646,6 +10651,7 @@ Tabs.Barb = {
                        GM_setValue('DF_' + Seed.player['name'] + '_city_' + counter + '_' + getServerId(), JSON2.stringify(t.barbArray[counter]));
                        saveAttackOptions();
                      }
+                     if(rslt.user_action)t.waittime = Number(unsafeWindow.unixtime()+400);
                      document.getElementById('pberror2').innerHTML = 'Excess Traffic errors:' + AttackOptions.BarbsFailedTraffic;
                      document.getElementById('pberror3').innerHTML = 'Rally Point errors: '+ AttackOptions.BarbsFailedRP;
                      document.getElementById('pberror4').innerHTML = 'Knight errors:' + AttackOptions.BarbsFailedKnight;
