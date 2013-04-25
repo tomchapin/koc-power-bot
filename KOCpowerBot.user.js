4dccd4b25834c82e7d189bd2f2f6a8f7dfd5b4ba
baos780@gmail.com <baos780@gmail.com@91a980a7-b36c-2723-9ba7-67aa146be212>
2013-04-25 10:09:06 +0000

    addition to whisper logger
    git-svn-id: http://koc-power-bot.googlecode.com/svn/trunk@1409 91a980a7-b36c-2723-9ba7-67aa146be212

--- KOCpowerBot.user.js
+++ KOCpowerBot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           KOC Power Bot
-// @version        201304125e
+// @version        201304125f
 // @namespace      mat
 // @homepage       http://userscripts.org/scripts/show/101052
 // @include        *.kingdomsofcamelot.com/*main_src.php*
@@ -34,7 +34,7 @@ if(window.self.location != window.top.location){
    }
 }
 
-var Version = '20130425e';
+var Version = '20130425f';
 
 //bandaid to stop loading in advertisements containing the @include urls
 if(document.URL.indexOf('sharethis') != -1) {
@@ -13129,6 +13129,7 @@ function eraseWhisper () {
 
 function whisperlog(chatwrap) {
 	if(!chatwrap) {
+		readWhisper();
      var catchwhispers = new CalterUwFunc ('Chat.getChat', [[/linkComment\)\;if/,'linkComment\)\;if(i==3)whisperlog(chatwrap);if']]);
      catchwhispers.setEnable(true);
 	} else {
