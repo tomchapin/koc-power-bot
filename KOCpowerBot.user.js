1e83748ab24809acf55efc49c2c619b367869590
baos780@gmail.com <baos780@gmail.com@91a980a7-b36c-2723-9ba7-67aa146be212>
2013-05-27 01:43:32 +0000

    forgot version increment
    git-svn-id: http://koc-power-bot.googlecode.com/svn/trunk@1461 91a980a7-b36c-2723-9ba7-67aa146be212

--- KOCpowerBot.user.js
+++ KOCpowerBot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           KOC Power Bot
-// @version        20130526c
+// @version        20130526d
 // @namespace      mat
 // @homepage       http://userscripts.org/scripts/show/101052
 // @include        *.kingdomsofcamelot.com/*main_src.php*
@@ -34,7 +34,7 @@ if(window.self.location != window.top.location){
    }
 }
 
-var Version = '20130526c';
+var Version = '20130526d';
 
 //bandaid to stop loading in advertisements containing the @include urls
 if(document.URL.indexOf('sharethis') != -1) {
