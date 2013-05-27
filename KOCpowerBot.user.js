ed8a835cddbb4fcc49b2c02a5d520f1809d1e912
baos780@gmail.com <baos780@gmail.com@91a980a7-b36c-2723-9ba7-67aa146be212>
2013-05-27 00:36:12 +0000

    kofcmon ajax upgrade
    git-svn-id: http://koc-power-bot.googlecode.com/svn/trunk@1459 91a980a7-b36c-2723-9ba7-67aa146be212

--- KOCpowerBot.user.js
+++ KOCpowerBot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           KOC Power Bot
-// @version        20130526B
+// @version        20130526c
 // @namespace      mat
 // @homepage       http://userscripts.org/scripts/show/101052
 // @include        *.kingdomsofcamelot.com/*main_src.php*
@@ -34,7 +34,7 @@ if(window.self.location != window.top.location){
    }
 }
 
-var Version = '20130526b';
+var Version = '20130526c';
 
 //bandaid to stop loading in advertisements containing the @include urls
 if(document.URL.indexOf('sharethis') != -1) {
@@ -21579,9 +21579,12 @@ function Sendtokofcmon (mapdata) {
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     },
     data: implodeUrlArgs(params),
-        onload: function (rslt) {
-            if(rslt.status != 200) throttle = 0;
-            else throttle = Number(rslt.responseText);
+        onload: function (message) {
+            if(message.status != 200) throttle = 0;
+            else {
+      			var rslt = eval("(" + message.responseText + ")");
+            	if(rslt.ok)throttle = Number(rslt.throttle);
+				};
 		  },
     })
 };
