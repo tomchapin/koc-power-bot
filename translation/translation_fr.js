76726134b0c407a16a68459fa161c67c81acbd71
barbarossa69@gmail.com <barbarossa69@gmail.com@91a980a7-b36c-2723-9ba7-67aa146be212>
2014-09-12 13:57:15 +0000

    git-svn-id: http://koc-power-bot.googlecode.com/svn/trunk@1767 91a980a7-b36c-2723-9ba7-67aa146be212

--- translation/translation_fr.js
+++ translation/translation_fr.js
@@ -1,18 +1,18 @@
-{
+ï»¿{
 "Version":"20140912a",
 "curlang":"fr",
 "9":"9",
 "10":"10", 
 "11":"11", 
 "12":"12", 
-"Stop Sound Alert":"Arrêtez alerte sonore", 
+"Stop Sound Alert":"ArrÃªtez alerte sonore", 
 "Play Sound Alert Now":"Jouer Alerte sonore maintenant", 
 "SETUP":"installation", 
-"Minimum number of troops to trigger tower options":"Nombre minimum de troupes pour déclencher des options tour", 
-"Automatically post incoming attacks to alliance chat":"Publiez automatiquement les attaques entrantes à l'alliance chat", 
-"Message Prefix":"Préfixe de message", 
+"Minimum number of troops to trigger tower options":"Nombre minimum de troupes pour dÃ©clencher des options tour", 
+"Automatically post incoming attacks to alliance chat":"Publiez automatiquement les attaques entrantes Ã  l'alliance chat", 
+"Message Prefix":"PrÃ©fixe de message", 
 "Alert on scouting":"Alerter sur le scoutisme", 
 "Alert on wild attack":"Alerter sur l'attaque sauvage", 
-"Display defend status":"Afficher défendre le statut",
+"Display defend status":"Afficher dÃ©fendre le statut",
 "Country":"Pays"
 }
